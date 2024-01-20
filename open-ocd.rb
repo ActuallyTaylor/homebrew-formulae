475407ab9a0eeeace6e97286bfdeed92bfa261da
Taylor Lineman <taylor.lineman@gmail.com>
2024-01-20 14:02:22 -0500

    Create open-ocd.rb

---
+++ open-ocd.rb
@@ -0,0 +1,41 @@
+class OpenOcd < Formula
+  desc "On-chip debugging, in-system programming and boundary-scan testing"
+  homepage "https://openocd.org/"
+  url "https://downloads.sourceforge.net/project/openocd/openocd/0.11.0/openocd-0.11.0.tar.bz2"
+  license "GPL-2.0-or-later"
+  revision 1
+
+  livecheck do
+    url :stable
+    regex(%r{url=.*?/openocd[._-]v?(\d+(?:\.\d+)+)\.t}i)
+  end
+
+  head do
+    url "https://github.com/openocd-org/openocd.git", tag: "v0.11.0"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+    depends_on "texinfo" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "capstone"
+  depends_on "hidapi"
+  depends_on "libftdi"
+  depends_on "libusb"
+
+  def install
+    ENV["CCACHE"] = "none"
+
+    system "./bootstrap", "nosubmodule" if build.head?
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--enable-buspirate",
+                          "--enable-stlink",
+                          "--enable-dummy",
+                          "--enable-jtag_vpi",
+                          "--enable-remote-bitbang"
+    system "make", "install"
+  end
+end
